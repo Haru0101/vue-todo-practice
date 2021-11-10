c74109f58bab8e19f7dda4f9d0d9fe38dd7a0b4f
Haru0101 <haruki.for.01@gmail.com>
2021-11-10 19:08:18 +0900

    option api → composition api 置換
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,9 +3,12 @@
         <h1>TODO LIST</h1>
         <!-- 送信して違うページに行かないようにサブミットを制御 -->
         <form v-on:submit.prevent>
-            <label for="name">名前</label><input id="name" type="text" v-model="newItem.name"><br>
-            <label for="category">カテゴリ<input type="text" id="category" v-model="newItem.category"></label><br>
-            <label for="deadline">締め切り<input type="text" id="deadline" v-model="newItem.deadline"></label><br>
+            <label for="name">名前</label><input id="name" type="text" v-model="newItem.name">
+            <br>
+            <label for="category">カテゴリ<input type="text" id="category" v-model="newItem.category"></label>
+            <br>
+            <label for="deadline">締め切り<input type="text" id="deadline" v-model="newItem.deadline"></label>
+            <br>
             <button @click="addItem">Add</button>
             <h2>未完了のタスク</h2>
             <ul>
@@ -34,82 +37,78 @@
     </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                newItem: {
-                    name: null,
-                    category: null,
-                    deadline: null
-                },
-                items: [],
-                completedItems: []
-            }
-        },
-        mounted() {
-            this.fetchItem();
-        },
-        updated() {
-            this.updateStorage();
-        },
-        methods: {
-            addItem: function () {
-                if (this.newItem.name == null) return;
-                // オブジェクトをプッシュするときは変数に入れるとスッキリする
-                let item = {
-                    name: this.newItem.name,
-                    category: this.newItem.category,
-                    deadline: this.newItem.deadline,
-                    isDone: false
-                };
+import { reactive } from 'vue';
+export default {
+    setup() {
+        const newItem = reactive<{ NewItem: Object }>([{name: null},{category: null}, {deadline: null}]);
+        const items = reactive<{Item: Array}>([]);
+        const completedItems = reactive<{CompletedItem: Array}>([]);
 
-                this.items.push(item);
-                console.log(this.items);
-                // this.updateStorage();
-                console.log(localStorage);
-                this.newItem.name = null;
-                this.newItem.category = null;
-                this.newItem.deadline = null;
-                this.updateStorage();
-                // this.fetchItem();
-            },
-            fetchItem: function () {
-                console.log('fetchItem');
-                console.log(localStorage.length);
-                // ローカルストレージからアイテムをフェッチする関数
-                this.items = JSON.parse(localStorage.getItem('items'));
-                this.completedItems = JSON.parse(localStorage.getItem('completedItems'));
-                if (!this.items) {
-                    this.items = [];
-                }
-                if (!this.completedItems) {
-                    this.completedItems = [];
-                }
-            },
-            resetStorage: function () {
-                localStorage.clear();
-                this.fetchItem();
-            },
-            updateStorage: function () {
-                console.log('updateStorage');
-                localStorage.setItem('items', JSON.stringify(this.items));
-                localStorage.setItem('completedItems', JSON.stringify(this.completedItems));
-            },
-            deleteItemChecked: function () {
-                // isDoneがtrueのものだけがreturn trueされ、新たな配列として格納される
-                this.items = this.items.filter(function (item) {
-                    return item.isDone === false;
-                });
-            },
-            completeItem: function () {
-                let completedItems = this.items.filter(function (item) {
-                    return item.isDone === true;
-                });
-                this.completedItems.push(...completedItems);
-                this.deleteItemChecked();
+        const addItem = () => {
+            if (this.newItem.name == null) return;
+            // オブジェクトをプッシュするときは変数に入れるとスッキリする
+            let item = {
+                name: this.newItem.name,
+                category: this.newItem.category,
+                deadline: this.newItem.deadline,
+                isDone: false
+            };
+
+            this.items.push(item);
+            console.log(this.items);
+            // this.updateStorage();
+            console.log(localStorage);
+            this.newItem.name = null;
+            this.newItem.category = null;
+            this.newItem.deadline = null;
+            this.updateStorage();
+            // this.fetchItem();
+        }
+
+        const fetchItem = () => {
+            console.log('fetchItem');
+            console.log(localStorage.length);
+            // ローカルストレージからアイテムをフェッチする関数
+            this.items = JSON.parse(localStorage.getItem('items'));
+            this.completedItems = JSON.parse(localStorage.getItem('completedItems'));
+            if (!this.items) {
+                this.items = [];
+            }
+            if (!this.completedItems) {
+                this.completedItems = [];
             }
         }
-    }
+
+        const resetStorage = () => {
+            localStorage.clear();
+            this.fetchItem();
+        }
+
+        const updateStorage = () => {
+            console.log('updateStorage');
+            localStorage.setItem('items', JSON.stringify(this.items));
+            localStorage.setItem('completedItems', JSON.stringify(this.completedItems));
+        }
+
+        const deleteItemChecked = () => {
+            // isDoneがtrueのものだけがreturn trueされ、新たな配列として格納される
+            this.items = this.items.filter(function (item) {
+                return item.isDone === false;
+            });
+        }
+
+        const completeItem = () => {
+            let completedItems = this.items.filter(function (item) {
+                return item.isDone === true;
+            });
+            this.completedItems.push(...completedItems);
+            this.deleteItemChecked();
+        }
+
+        return {newItem, items, completedItems, addItem, fetchItem,resetStorage, updateStorage, deleteItemChecked, completeItem};
+
+    },
+}
 </script>
 
 <style>
